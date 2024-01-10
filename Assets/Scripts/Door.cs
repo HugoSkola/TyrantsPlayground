11434360617e06ceddff5fee3ae07ae679a99175
Colorblind546 <morgan.marinbeishline@elev.ga.lbs.se>
2024-01-10 15:07:08 +0100

    Door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    bool byDoor;
+    int doorInstance;
+    public Sprite[] doorInstanceSprite;
+    public SpriteRenderer doorType;
     public GameObject door;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +18,7 @@ public class Door : MonoBehaviour
     // Called when the player is touching a door
     private void OnTriggerStay2D(Collider2D collision)
     {
+        byDoor = collision;
         DoorUse doorUse = collision.GetComponent<DoorUse>();
         doorUse.InDoor(door);
     }
@@ -21,6 +26,9 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (byDoor && doorInstance != 2)
+        {
+            
+        }
     }
 }
