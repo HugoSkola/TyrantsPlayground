8e13027b9e2461801d56212652462c23562b4656
Colorblind546 <morgan.marinbeishline@elev.ga.lbs.se>
2024-01-11 13:45:22 +0100

    Removed useless bits of code

--- Assets/Scripts/DoorUse.cs
+++ Assets/Scripts/DoorUse.cs
@@ -6,7 +6,6 @@ public class DoorUse : MonoBehaviour
 {
     GameObject publicDoor;
     bool doorActive;
-    public GameObject correspondingDoor;
     // Start is called before the first frame update
     void Start()
     {
