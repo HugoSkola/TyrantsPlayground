01149b92ccdf1c9d501952f28d7fc622d9011863
HugoSkola <151546996+HugoSkola@users.noreply.github.com>
2024-01-12 16:06:44 +0100

    Fixat så att ljud kommer när man går/skjuta

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -14,35 +14,44 @@ public class Shooter : MonoBehaviour
     float shootingDelay = 1f;
 
     float nextFireTime = 0f;
+    
+    
+    public AudioClip ShootingSound;
 
+    private AudioSource audioSource;
+    
     Transform spawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time >= nextFireTime)
+        if (Input.GetKeyDown(KeyCode.F) && Time.time >= nextFireTime)
         {
             ShootBullet();
-            nextFireTime = Time.time + 1f / shootingDelay;
         }
-        
     }
 
     void ShootBullet()
     {
         GameObject bullet = Instantiate(BulletPrefab, bulletspawn.position, Quaternion.identity);
-
+        
         bool isFacingLeft = transform.localScale.x < 0;
 
         if (!isFacingLeft)
         {
             bullet.transform.Rotate(0f, 180f, 0f);
+        }
+
+        nextFireTime = Time.time + 1f / shootingDelay;
+
+        if (ShootingSound != null)
+        {
+            audioSource.PlayOneShot(ShootingSound);
         }
     }
 }
