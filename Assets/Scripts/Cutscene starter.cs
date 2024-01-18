e4d820d401b346b6e55555d62358ddf72f821803
Leonelhermano <leon.velandiajaime@elev.ga.lbs.se>
2024-01-18 23:55:25 +0100

    level 2

--- Assets/Scripts/Cutscene starter.cs
+++ Assets/Scripts/Cutscene starter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,7 @@ public class Cutscenestarter : MonoBehaviour
     void Start()
     {
         HideSpriteCutscene();
+        Console.WriteLine("hej");
     }
     
     // Update is called once per frame
---
+++ Assets/Scripts/catapultscene.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catapultscene : MonoBehaviour
+{
+    private void Start()
+    {
+        Console.WriteLine("amongus");
+    }
+    
+}
