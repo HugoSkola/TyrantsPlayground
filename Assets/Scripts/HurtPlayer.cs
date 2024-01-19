f511dcf39d49e17ad7e50c58803dcb6957f890e1
Colorblind546 <morgan.marinbeishline@elev.ga.lbs.se>
2024-01-19 13:11:04 +0100

    enemy attack animation fixed

--- Assets/Scripts/HurtPlayer.cs
+++ Assets/Scripts/HurtPlayer.cs
@@ -8,9 +8,12 @@ public class HurtPlayer : MonoBehaviour
     int hurtAmount = 1;
     public GameObject attackAnim;
     SpriteRenderer sr;
+    public float countdown;
+    public float countdownReset;
 
     void Start()
     {
+        countdown = 0.1f;
         attackAnim.SetActive(false);
         sr = GetComponent<SpriteRenderer>();
     }
@@ -18,15 +21,24 @@ public class HurtPlayer : MonoBehaviour
     private void OnCollisionStay2D(Collision2D collision)
     {
         PlayerHealth phealth = collision.gameObject.GetComponent<PlayerHealth>();
-        if (phealth == null)
+        if (phealth != null && countdown < 0f)
         {
             attackAnim.SetActive(true);
             sr.enabled = false;
+            phealth.Hurt(hurtAmount);
             Invoke("ResetAnimation", 0.5f);
+            countdown = countdownReset;
             return;
         }
+        else if (phealth == null)
+        {
+            return;
+        }
+        else
+        {
+            countdown -= Time.deltaTime;
+        }
 
-        phealth.Hurt(hurtAmount);
     }
 
     private void ResetAnimation()
