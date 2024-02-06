6f9e74ba1325efd892c069cc07e5464749734d50
Leonelhermano <leon.velandiajaime@elev.ga.lbs.se>
2024-02-06 13:59:19 +0100

    we

--- Assets/bosshealth.cs
+++ Assets/bosshealth.cs
@@ -1,18 +1,23 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class bosshealth : MonoBehaviour
 {
+    public Image bossbar;
+    [SerializeField]
+    public int bosshp = 100;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bossbar = GameObject.Find("bossbar").GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bossbar.fillAmount = bosshp / 100f;
     }
 }
